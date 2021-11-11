89a7e5e64c3b557cec88ea11a7311c7b2fdf1ab3
Sywarr-awadhi <57920802+Sywarr-awadhi@users.noreply.github.com>
2021-11-11 09:04:05 +0100

    Update RestControlEmploye.java

--- src/main/java/tn/esprit/spring/controller/RestControlEmploye.java
+++ src/main/java/tn/esprit/spring/controller/RestControlEmploye.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import tn.esprit.spring.dto.ContratDTO;
 import tn.esprit.spring.dto.EmployeDTO;
 import tn.esprit.spring.entities.Contrat;
